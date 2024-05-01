e582ae5b239e210de8fd04275e66fd538fcc1286
Christian Garcia <christiangarci6@gmail.com>
2024-05-01 03:54:53 -0500

    first commit

---
+++ parameters.py
@@ -0,0 +1,6 @@
+file_name = 'connections.csv'
+linkedin_username = 'christiangarci6@gmail.com'
+linkedin_password = 'Cgarci6!Gabriel'
+keywords = 'DevOps,Linux,Python,AWS,Software Engineering,CEO,Founder'
+ignore_list = 'Person1,Person2'
+till_page = 10
